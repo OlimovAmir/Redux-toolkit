cf7904e727a5e9fe6f35920104f5ecf1b9a8af27
olimov Amir <olimov.amir@mail.ru>
2023-11-14 12:40:13 +0300

    add icon in field input

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,7 +3,8 @@ import styles from './Header.module.css';
 import { ROUTES } from './../../utils/routes';
 import { Link } from 'react-router-dom';
 import LOGO from '../../img/logo_it.jpg';
-import '@fortawesome/fontawesome-free/css/all.min.css'; // Добавьте эту строку
+import AVATAR from '../../img/robot.jpg'
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
 function Header() {
@@ -14,16 +15,34 @@ function Header() {
       </div>
       <div className={styles.info}>
         <div className={styles.user}>
-          <div className={styles.avatar}></div>
+          <div className={styles.avatar}> <img src={AVATAR} alt="avatar" /> </div>
           <div className={styles.username} >Guest</div>
         </div>
-
       </div>
       <form className={styles.form}>
-        <div className={styles.icon} > <i class="fa-solid fa-magnifying-glass"></i> </div>
-        <input type="search" />
-        <button type='button' >Search</button>
+        <div className={styles.searchContainer}>
+        <i className="fa-solid fa-magnifying-glass"></i>
+          <input
+            type="search"
+            name='search'
+            placeholder='Search'
+            autoComplete='off'
+            onChange={() => { }}
+            value=''
+          />
+          
+        </div>
+        <div className={styles.box}> </div>
+        <button type='button' >
+          <div className={styles.icon} >
+             Search 
+          </div></button>
       </form>
+      <div className={styles.account}>
+        <Link to={ROUTES.HOME} className={styles.icon} > <i class="fa-regular fa-heart"></i> </Link>
+        <Link to={ROUTES.CART} className={styles.cartIcon}> <i class="fa-solid fa-cart-shopping"></i> </Link>
+        <span className={styles.count}>2</span>
+      </div>
     </div>
   )
 }
