e0875441013c2c2b23572f2ddd7933dc2187ab46
olimov Amir <olimov.amir@mail.ru>
2023-11-14 08:47:07 +0300

    add logo

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import styles from './Header.module.css';
 import { ROUTES } from './../../utils/routes';
 import { Link } from 'react-router-dom';
+import LOGO from '../../img/logo_it.jpg'
 
 function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <Link to={ROUTES.HOME} >My courses</Link>
+        <Link to={ROUTES.HOME} >
+          <img src={LOGO} alt="logo" />
+        </Link>
       </div>
     </div>
   )
