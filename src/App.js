a3feeed81a6ef5961622d1da49ea47e63b41771b
olimov Amir <olimov.amir@mail.ru>
2023-11-14 08:38:02 +0300

    add link home

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import AppRoutes from './components/Routes/Routes';
 import './styles/App.css';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Sidebar from './components/sidebar/Sidebar';
+import AppStyles from './styles/App.module.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <AppRoutes />
+      <div className={AppStyles.container}>
+        <Sidebar/>
+        <AppRoutes />
+      </div>
+      
       <Footer/>
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import styles from './Header.module.css';
+import { ROUTES } from './../../utils/routes';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className={styles.header}>
+      <div className={styles.logo}>
+        <Link to={ROUTES.HOME} >My courses</Link>
+      </div>
+    </div>
   )
 }
 
