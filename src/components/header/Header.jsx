3db15dcc1eed27666e189eaaf72de41af65fc89b
olimov Amir <olimov.amir@mail.ru>
2023-11-14 09:41:26 +0300

    add icon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,16 +2,28 @@ import React from 'react';
 import styles from './Header.module.css';
 import { ROUTES } from './../../utils/routes';
 import { Link } from 'react-router-dom';
-import LOGO from '../../img/logo_it.jpg'
+import LOGO from '../../img/logo_it.jpg';
+import '@fortawesome/fontawesome-free/css/all.min.css'; // Добавьте эту строку
+
 
 function Header() {
   return (
     <div className={styles.header}>
       <div className={styles.logo}>
-        <Link to={ROUTES.HOME} >
-          <img src={LOGO} alt="logo" />
-        </Link>
+        <Link to={ROUTES.HOME} > <img src={LOGO} alt="logo" /> </Link>
       </div>
+      <div className={styles.info}>
+        <div className={styles.user}>
+          <div className={styles.avatar}></div>
+          <div className={styles.username} >Guest</div>
+        </div>
+
+      </div>
+      <form className={styles.form}>
+        <div className={styles.icon} > <i class="fa-solid fa-magnifying-glass"></i> </div>
+        <input type="search" />
+        <button type='button' >Search</button>
+      </form>
     </div>
   )
 }
