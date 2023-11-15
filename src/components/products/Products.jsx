52e303ad6f5fbcba020e921d280b94a426fb3011
olimov Amir <olimov.amir@mail.ru>
2023-11-15 08:39:54 +0300

    created components Products

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import stylesProduct from './Products.module.css'
+import { Link } from 'react-router-dom'
+
+function Products({ title, products = [], amount, style = {} }) {
+    const list = products.filter((_, i)=> i < amount)
+    return (
+        <section className={stylesProduct.products} style={style}>
+            {title && <h2>{title}</h2>}
+            <div className={stylesProduct.list}>
+                {list.map(({
+                    id,
+                    images,
+                    title,
+                    category: { name: cat },
+                    price,
+                }) =>
+                    <Link to={`/products/${id}`} key={id} className={stylesProduct.product}>
+                        <div
+                            className={stylesProduct.image}
+                            style={{ backgroundImage: `url(${images[0]})` }}
+                        > </div>
+                        <div className={stylesProduct.wrapper} >
+                            <h3 className={stylesProduct.title}>{title}</h3>
+                            <div className={stylesProduct.cat}>{cat}</div>
+                            <div className={stylesProduct.info}>
+                                <div className={stylesProduct.prices}>
+                                    <div className={stylesProduct.price}>{price}$</div>
+                                    <div className={stylesProduct.oldPrice}>{Math.floor(price * 0.8)}$</div>
+                                </div>
+                                <div className={stylesProduct.purchases}>
+                                    {Math.floor(Math.random() * 20 +1 )} purchased
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                )}
+            </div>
+        </section>
+    )
+}
+
+export default Products
